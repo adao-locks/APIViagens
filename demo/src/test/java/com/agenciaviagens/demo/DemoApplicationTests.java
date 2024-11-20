3f9f1883c20dcb8300d1849dc19b022d285f98cf
Eduardo Adão Locks <122515706+adao-locks@users.noreply.github.com>
2024-11-20 13:38:37 -0300

    first commit

---
+++ demo/src/test/java/com/agenciaviagens/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenciaviagens.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
